507ac851ea6264cd391d3ac5f77ae090de39581a
Tengttttt <60600551+Tengttttt@users.noreply.github.com>
2020-02-20 17:29:28 +0800

    Update part_1.py
    修改绿线消失的问题

--- ShiChuang/lib/part_1.py
+++ ShiChuang/lib/part_1.py
@@ -105,31 +105,34 @@ class part_1_:
         dot[0] = pygame.draw.circle(self.screen, RED, pos[0], self.r)
         for i in range(1, len(dot)):
             dot[i] = pygame.draw.circle(self.screen, GOLD, pos[i], self.r)
-        for event in pygame.event.get():
 
-            if event.type == QUIT:
-                exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+        while True:
+            for event in pygame.event.get():
+
+                if event.type == QUIT:
                     exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for i in dot:
-                    if i.left <= event.pos[0] <= i.left + i.width and i.top <= event.pos[1] <= i.top + i.height:
-                        self.pair_dot[1] = dot.index(i)
-                        # 判断鼠标点击的点是否与上个点相邻，如果相邻，就执行函数，将两点之间的线染色，并且计数器加1
-                        if self.pair_dot[1] in list(nx.neighbors(self.g, self.pair_dot[0])):
-                            self.still = True
-                            self.counter = self.counter + 1
-                            # 从所有边集中移除走过的边，要判断是因为有的边是重复走过，之前就已经删除
-                            if set(self.pair_dot) in self.edge_judge:
-                                self.edge_judge.remove(set(self.pair_dot))
-                            # 如果走回到起始点，判断是否走过所有边，如果已经走完，结束游戏，输出计数器
-                            if self.pair_dot[1] == 0:
-                                if len(self.edge_judge) == 0:
-                                    print(self.counter)
-                                    exit()
-                        self.pair_dot = change_line_color(self.screen, self.still, self.pair_dot)
-                        self.still = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in dot:
+                        if i.left <= event.pos[0] <= i.left + i.width and i.top <= event.pos[1] <= i.top + i.height:
+                            self.pair_dot[1] = dot.index(i)
+                            # 判断鼠标点击的点是否与上个点相邻，如果相邻，就执行函数，将两点之间的线染色，并且计数器加1
+                            if self.pair_dot[1] in list(nx.neighbors(self.g, self.pair_dot[0])):
+                                self.still = True
+                                self.counter = self.counter + 1
+                                # 从所有边集中移除走过的边，要判断是因为有的边是重复走过，之前就已经删除
+                                if set(self.pair_dot) in self.edge_judge:
+                                    self.edge_judge.remove(set(self.pair_dot))
+                                # 如果走回到起始点，判断是否走过所有边，如果已经走完，结束游戏，输出计数器
+                                if self.pair_dot[1] == 0:
+                                    if len(self.edge_judge) == 0:
+                                        print(self.counter)
+                                        exit()
+                            self.pair_dot = change_line_color(self.screen, self.still, self.pair_dot)
+                            self.still = False
+            pygame.display.update()
 
         # print(TEST)
         # for i in range(2, 11):
